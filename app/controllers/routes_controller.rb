ee7f6831ca70b3c031243bf0fb41ad41e7c9a53f
radville <laura.radville@gmail.com>
2020-03-07 14:15:01 -0500

    moved getting routes data to Route model

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,14 +1,7 @@
 class RoutesController < ApplicationController
     def index
         # get route data from MBTA API
-        routes_response = RestClient.get("https://api-v3.mbta.com/routes?filter[type]=0,1&api_key=#{ENV['MBTA_KEY']}")
-
-        routes_data = JSON.parse(routes_response)["data"]
-
-        # Create Routes from the parsed data
-        routes_data.map do |route|
-            Route.create(name: route["attributes"]["long_name"], mbta_id: route["id"])
-        end
+        Route.get_data
 
         # Display the route names at path /routes
         @routes = Route.all.map{ |route| route.name }
--- app/models/route.rb
+++ app/models/route.rb
@@ -3,4 +3,21 @@ class Route < ApplicationRecord
     has_many :stops, through: :route_stops
 
     validates_uniqueness_of :mbta_id
+
+    # get routes data from MBTA API
+    def self.get_data
+        routes_response = RestClient.get("https://api-v3.mbta.com/routes?filter[type]=0,1&api_key=#{ENV['MBTA_KEY']}")
+
+        routes_data = JSON.parse(routes_response)["data"]
+
+        # Create Routes from the parsed data
+        routes_data.map do |route|
+            Route.create(name: route["attributes"]["long_name"], mbta_id: route["id"])
+        end
+    end
+
+    # Calculate Route with most stops
+    def self.most 
+        self.sort_by{ |route| route.stops.length }[0]
+    end
 end
