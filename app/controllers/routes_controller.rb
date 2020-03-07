4d0e4770cea9fe92506918c53989ec5b7d117bf1
radville <laura.radville@gmail.com>
2020-03-07 13:09:20 -0500

    /routes show all light and heavy rail routes

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,6 +1,18 @@
 class RoutesController < ApplicationController
     def index
-        @routes = Route.all
+        routes_response = RestClient.get("https://api-v3.mbta.com/routes?filter[type]=0,1&api_key=#{ENV['MBTA_KEY']}")
+
+        routes_data = JSON.parse(routes_response)["data"]
+
+        # Display the route names at path /routes
+        @routes = routes_data.map do |route|
+            route["attributes"]["long_name"]
+        end
+
+        # List the route names in the console
+        @routes.each do |route|
+            puts @routes
+        end
 
     end
 end
