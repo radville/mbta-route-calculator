430e91937ee397682ef9439d01cb279821a4b090
radville <laura.radville@gmail.com>
2020-03-08 14:06:34 -0400

    removed unsed code and added comments

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -4,6 +4,7 @@ class Stop < ApplicationRecord
 
     # validates_uniqueness_of :name
 
+    # return stops that connect 2 or more routes
     def self.with_connections
         stops = []
 
@@ -16,14 +17,14 @@ class Stop < ApplicationRecord
         stops.sort_by!{ |s| s.name }
     end
 
+    # given the params from the search page, find the two stops
     def self.get_stops_from_params(params)
         @stop1 = Stop.find(params["stop_1"]["stop_id"])
         @stop2 = Stop.find(params["stop_2"]["stop_id"])
     end
 
-    def self.connect_stops(params)
-        self.get_stops_from_params(params)
-
+    # find the route that connects two stops
+    def self.connect_stops
         # if don't need any transfers
         if @stop1.routes.find { |route| @stop2.routes.include?(route) }
             @stop1.routes.find { |route| @stop2.routes.include?(route) }.name
@@ -36,13 +37,15 @@ class Stop < ApplicationRecord
         # if need two transfers
         else
             # add case where there need to be 2 or more transfers
+            "TDB"
         end
         
     end
 
+    # display the two stops and their connecting route
     def self.display_connections(params)
         self.get_stops_from_params(params)
-        @stop1.name + " to " + @stop2.name + " --> " + Stop.connect_stops(params)
+        @stop1.name + " to " + @stop2.name + " --> " + Stop.connect_stops
     end
 
 end
